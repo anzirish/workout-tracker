49e0c96c247625c1175a51bd48cc4367156f37ac
Vipin Dev <anzirish@gmail.com>
2026-03-03 19:33:18 +0530

    feat: add API response util functions

---
+++ backend/src/utils/apiResponse.js
@@ -0,0 +1,12 @@
+export const successReponse = (
+  res,
+  data = [],
+  message = "Success",
+  statusCode = 200,
+) => {
+  return res.status(statusCode).json({ success: true, message, data });
+};
+
+export const errorResponse = (res, message, statusCode = 500) => {
+  return res.status(statusCode).json({ success: false, message });
+};
