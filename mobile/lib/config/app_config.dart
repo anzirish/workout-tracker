69613430522b612bc5cf32810f3b73901a3b11fa
Vipin Dev <anzirish@gmail.com>
2026-03-04 07:56:08 +0530

    feat: add API configuration

---
+++ mobile/lib/config/app_config.dart
@@ -0,0 +1,7 @@
+class AppConfig {
+  // For Chrome/Web: localhost
+  static const String baseUrl = 'http://localhost:3000/api';
+
+  // Hardcoded user ID for testing
+  static const int userId = 1;
+}
