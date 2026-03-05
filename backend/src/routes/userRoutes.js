032441ca45721613798afb823160845cf8f5c920
Vipin Dev <anzirish@gmail.com>
2026-03-05 07:40:46 +0530

    feat: add validation middlewares

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,13 +1,30 @@
-import express from "express"
-import { getStreak, getWorkoutHistory, workoutComplete } from "../controllers/userController.js"
+import express from "express";
+import {
+  getStreak,
+  getWorkoutHistory,
+  workoutComplete,
+} from "../controllers/userController.js";
+import { protect } from "../middlewares/authMiddleware.js";
+import {
+  completeWorkoutValidator,
+  userIdValidator,
+} from "../utils/userValidator.js";
+import { validate } from "../middlewares/validationMiddleware.js";
 
-export const userRouter = express.Router()
+export const userRouter = express.Router();
+
+userRouter.use(protect);
 
 // GET /api/users/:userId/workout-history
-userRouter.get("/:userId/workout-history", getWorkoutHistory)
+userRouter.get("/:userId/workout-history", userIdValidator, validate, getWorkoutHistory);
 
 // POST /api/users/:userId/workouts/:workoutId/complete
-userRouter.post("/:userId/workouts/:workoutId/complete", workoutComplete)
+userRouter.post(
+  "/:userId/workouts/:workoutId/complete",
+  completeWorkoutValidator,
+  validate,
+  workoutComplete
+);
 
 // GET /api/users/:userId/streak
-userRouter.get("/:userId/streak", getStreak)
+userRouter.get("/:userId/streak", userIdValidator, validate, getStreak);
